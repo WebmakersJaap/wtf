34fd282a1779c25c57aba68e2fbc722889d56868
Jaap Broeders <jaap@dewebmakers.nl>
2021-03-05 11:23:56 +0100

    Even easier

--- src/wtf.php
+++ src/wtf.php
@@ -6,18 +6,10 @@
  * Example: wtf('foo');
  * Example: wtf(['bar'], ['foo'=>'bar']);
  */
-namespace Wtf;
-
-class Wtf {
-    public static function wtf($x) {
+function wtf() {
+    array_map(function($x) {
         if(is_object($x)||is_array($x)) echo '<pre>'.print_r($x,1).'</pre>'.PHP_EOL;
         elseif(function_exists('var_dump_safe')) { var_dump_safe($x); echo '<br>'.PHP_EOL; }
         else { var_dump($x); echo '<br>'.PHP_EOL; }
-    }
-}
-
-function wtf() {
-    array_map(function($x) {
-        Wtf::wtf($x);
     }, func_get_args());
 }
